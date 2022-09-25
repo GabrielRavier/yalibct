e780f3e85b79ae6ba2d1302efc163b8c4fcd0597
Gabriel Ravier <gabravier@gmail.com>
2022-09-26 00:59:15 +0200

    feat: Added strlen/llvm-project-test

---
+++ tests/strlen/llvm-project-test.c
@@ -0,0 +1,30 @@
+//===-- Unittests for strlen ----------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "test-deps/llvm-project.h"
+#include <string.h>
+
+TEST(LlvmLibcStrLenTest, EmptyString) {
+  const char *empty = "";
+
+  size_t result = __llvm_libc strlen(empty);
+  ASSERT_EQ((size_t)0, result);
+}
+
+TEST(LlvmLibcStrLenTest, AnyString) {
+  const char *any = "Hello World!";
+
+  size_t result = __llvm_libc strlen(any);
+  ASSERT_EQ((size_t)12, result);
+}
+
+int main()
+{
+    LlvmLibcStrLenTestAnyString();
+    LlvmLibcStrLenTestEmptyString();
+}
