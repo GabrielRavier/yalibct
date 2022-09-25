9182c2e68335061e778f721befbfd60dab07cc9e
Gabriel Ravier <gabravier@gmail.com>
2022-09-25 17:15:10 +0200

    feat: Added strlen/open-posix-testsuite-1-1

---
+++ include/test-deps/open-posix-testsuite.h
@@ -0,0 +1,14 @@
+// Based on code with this license:
+/*
+ * Copyright (c) 2002, Intel Corporation. All rights reserved.
+ * Created by:  julie.n.fleischer REMOVE-THIS AT intel DOT com
+ * This file is licensed under the GPL license.  For the full content
+ * of this license, see the COPYING file at the top level of this
+ * source tree.
+ */
+
+#pragma once
+
+#define PTS_UNRESOLVED  2
+#define PTS_FAIL        1
+#define PTS_PASS        0
