f058f3bdee603093f50f0632b9d2af23655138cb
Gabriel Ravier <gabravier@gmail.com>
2022-10-02 18:00:26 +0200

    feat: Added strlen/scc-0018

---
+++ tests/strlen/scc-0018.c
@@ -0,0 +1,21 @@
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+
+/*
+output:
+end:
+*/
+
+int
+main()
+{
+	//puts("testing");
+
+	assert(strlen("01234") == 5);
+	assert(strlen("") == 0);
+
+	//puts("done");
+
+	return 0;
+}
