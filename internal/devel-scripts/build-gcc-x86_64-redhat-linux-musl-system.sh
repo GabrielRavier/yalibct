0419932a9469d94638e7cef59ddecfb65437b7c5
Gabriel Ravier <gabravier@gmail.com>
2024-08-25 18:34:27 +0200

    TBD (fixed musl-system build) squash this together with previous commits

--- internal/devel-scripts/build-gcc-x86_64-redhat-linux-musl-system.sh
+++ internal/devel-scripts/build-gcc-x86_64-redhat-linux-musl-system.sh
@@ -3,14 +3,10 @@ CC=musl-gcc \
     cmake \
         -B build/musl-system \
         -DCMAKE_C_FLAGS='-ggdb3' \
-        -DYALIBCT_LIBC_HAS_CKD_ADD=OFF \
-        -DYALIBCT_DISABLE_PRINTF_LOWERCASE_B_CONVERSION_SPECIFIER_TESTS=ON \
-        -DYALIBCT_DISABLE_PRINTF_UPPERCASE_B_CONVERSION_SPECIFIER_TESTS=ON \
-        -DYALIBCT_DISABLE_RARE_LOCALE_TESTS=ON \
-        -DYALIBCT_LIBC_HAS_FCVT=OFF \
-        -DYALIBCT_LIBC_HAS_ECVT=OFF \
-        -DYALIBCT_LIBC_HAS_WCSCMP=OFF \
-        -DYALIBCT_LIBC_HAS_WCSNCMP=OFF \
-        -DYALIBCT_LIBC_HAS_SWPRINTF=OFF \
+        -DYALIBCT_DISABLE_RARE_LOCALE_TESTS=ON `# musl doesn't have a bunch of locales and just pretends it does, so we can't check for them at runtime` \
+        -DYALIBCT_DISABLE_PRINTF_LOWERCASE_B_CONVERSION_SPECIFIER_TESTS=ON `# Not yet implemented` \
+        -DYALIBCT_DISABLE_PRINTF_UPPERCASE_B_CONVERSION_SPECIFIER_TESTS=ON `# Not yet implemented` \
+        -DYALIBCT_LIBC_HAS_FCVT=OFF `# See https://www.openwall.com/lists/musl/2022/09/06/10` \
+        -DYALIBCT_LIBC_HAS_ECVT=OFF `# See https://www.openwall.com/lists/musl/2022/09/06/4` \
 && \
     cmake --build build/musl-system --parallel 14
