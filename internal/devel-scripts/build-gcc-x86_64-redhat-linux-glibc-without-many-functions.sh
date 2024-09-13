bf6a7d13f19511b684a050d99bea343f23616709
Gabriel Ravier <gabravier@gmail.com>
2024-09-13 18:23:42 +0200

    TBD (misc changes to directory names) squash this together with previous commits

--- internal/devel-scripts/build-gcc-x86_64-redhat-linux-glibc-without-many-functions.sh
+++ internal/devel-scripts/build-gcc-x86_64-redhat-linux-glibc-without-many-functions.sh
@@ -2,11 +2,11 @@
 # TODO: make this pretend to not have functions that we have replacements for in test-lib
 CC=gcc \
     cmake \
-        -B build/gcc-without-many-functions \
+        -B build/gcc-glibc-without-many-functions \
         -DCMAKE_C_FLAGS='-ggdb3' \
         -DYALIBCT_LIBC_HAS_CKD_ADD=OFF \
         -DYALIBCT_LIBC_HAS_ECVT=OFF \
         -DYALIBCT_LIBC_HAS_O_SEARCH=OFF \
         -DYALIBCT_DISABLE_SNPRINTF_SIZE_LARGER_THAN_ACTUAL_BUFFER_TESTS=ON \
 && \
-    cmake --build build/gcc-without-many-functions --parallel 14
+    cmake --build build/gcc-glibc-without-many-functions --parallel 14
--- internal/devel-scripts/build-gcc-x86_64-redhat-linux-glibc.sh
+++ internal/devel-scripts/build-gcc-x86_64-redhat-linux-glibc.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 CC=gcc \
     cmake \
-        -B build/gcc \
+        -B build/gcc-glibc \
         -DCMAKE_C_FLAGS='-ggdb3' \
         -DYALIBCT_LIBC_HAS_O_SEARCH=OFF `# See https://sourceware.org/bugzilla/show_bug.cgi?id=18228` \
         -DYALIBCT_DISABLE_SNPRINTF_SIZE_LARGER_THAN_ACTUAL_BUFFER_TESTS=ON `# See https://sourceware.org/bugzilla/show_bug.cgi?id=30441` \
         -DYALIBCT_LIBC_HAS_ECVT=OFF `# See https://sourceware.org/bugzilla/show_bug.cgi?id=29556` \
 && \
-    cmake --build build/gcc --parallel 14
+    cmake --build build/gcc-glibc --parallel 14
 
