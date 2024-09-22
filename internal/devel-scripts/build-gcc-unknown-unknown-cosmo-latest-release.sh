b51d7be641e489723cca4271167894aee94320b5
Gabriel Ravier <gabravier@gmail.com>
2024-09-23 01:48:28 +0200

    fix: Some CMakeLists refactors, TODO and internal scripts updates
    This fixes a stupid bug in the CMakeLists.txt to avoid having all of
    the YALIBCT_DISABLE_ flags get enabled by default. This also removes
    the TODO entry corresponding to the previous commit, and updates an
    internal build script for cosmopolitan's latest release to use the
    latest release (released yesterday).

--- internal/devel-scripts/build-gcc-unknown-unknown-cosmo-latest-release.sh
+++ internal/devel-scripts/build-gcc-unknown-unknown-cosmo-latest-release.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-MODE=dbg CC='/home/gravier/src/libraries/programming/languages-implementations/standard-libraries/c/implementations/cosmopolitan-libc/.cosmocc/3.8.0/bin/cosmocc' \
+MODE=dbg CC='/home/gravier/src/libraries/programming/languages-implementations/standard-libraries/c/implementations/cosmopolitan-libc/.cosmocc/3.9.2/bin/cosmocc' \
     cmake \
         -B build/cosmopolitan-latest-release \
         -Werror=dev -Werror=deprecated --warn-uninitialized `# All of the CMake warning options that seem useful` \
@@ -17,16 +17,8 @@ MODE=dbg CC='/home/gravier/src/libraries/programming/languages-implementations/s
         -DYALIBCT_DISABLE_PRINTF_PRECISION_ON_WIDE_C_CONVERSION_SPECIFIER_TESTS=ON `# cosmopolitan has deliberately decided to deviate from the standard on this, see https://github.com/jart/cosmopolitan/pull/1271` \
         -DYALIBCT_DISABLE_PRINTF_UPPERCASE_B_CONVERSION_SPECIFIER_TESTS=ON `# (PR link TODO)` \
         -DYALIBCT_DISABLE_PRINTF_NUMBERED_ARGUMENTS_TESTS=ON `# Not yet implemented` \
-        -DYALIBCT_DISABLE_PRINTF_A_CONVERSION_SPECIFIER_TESTS=ON `# See https://github.com/jart/cosmopolitan/pull/1286, https://github.com/jart/cosmopolitan/pull/1287, https://github.com/jart/cosmopolitan/pull/1296, https://github.com/jart/cosmopolitan/pull/1297` \
-        -DYALIBCT_DISABLE_PRINTF_APOSTROPHE_FLAG_TESTS=ON `# See https://github.com/jart/cosmopolitan/pull/1287` \
         -DYALIBCT_DISABLE_SCANF_NUMBERED_ARGUMENTS_TESTS=ON `# Not yet implemented` \
-        -DYALIBCT_DISABLE_PRINTF_UPPERCASE_L_LENGTH_MODIFIER_TESTS=ON `# See https://github.com/jart/cosmopolitan/pull/1273 and https://github.com/jart/cosmopolitan/pull/1283` \
         -DYALIBCT_DISABLE_SCANF_LEFT_SQUARE_BRACKET_CONVERSION_SPECIFIER_TESTS=ON `# Not yet implemented` \
-        -DYALIBCT_LIBC_DOESNT_HAVE_FCVT=ON `# See https://github.com/jart/cosmopolitan/pull/1282` \
-        -DYALIBCT_LIBC_DOESNT_HAVE_ECVT=ON `# See https://github.com/jart/cosmopolitan/pull/1282` \
-        -DYALIBCT_DISABLE_PRINTF_N_CONVERSION_SPECIFIER_TESTS=ON `# See https://github.com/jart/cosmopolitan/pull/1282` \
-        -DYALIBCT_DISABLE_PRINTF_UPPERCASE_C_CONVERSION_SPECIFIER_TESTS=ON `# See https://github.com/jart/cosmopolitan/pull/1276` \
-        -DYALIBCT_DISABLE_PRINTF_ERRNO_TESTS=ON `# See https://github.com/jart/cosmopolitan/pull/1275` \
 && \
     MODE=dbg \
         cmake --build build/cosmopolitan-latest-release --parallel 14
